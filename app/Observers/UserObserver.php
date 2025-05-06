07f4575a075682341f40759b8be82be427b0906e
Edenilson Mota <edenilsonmota.job@gmail.com>
2025-05-05 22:27:01 -0300

    Criando Wallet Automaticamente com Observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use App\Models\Wallet;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        // Cria uma carteira associada ao usuário
+        Wallet::create([
+            'user_id' => $user->id,
+            'balance' => 0, // Inicializa com 0
+        ]);
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
