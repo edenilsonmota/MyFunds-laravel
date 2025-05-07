2cb8126950db55c60c042564f26a799daf1085c2
Edenilson Mota <edenilsonmota.job@gmail.com>
2025-05-06 23:34:13 -0300

    Fix: Corrigindo os alert para fecharem com 'x'

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,13 +1,16 @@
 @if (session('success'))
-    <div class="bg-green-100 text-green-800 px-4 py-2 rounded mb-4">
+    <div id="success-alert" class="bg-green-100 text-green-800 px-4 py-2 rounded mb-4 relative transition-opacity duration-300 ease-in-out">
+        <button onclick="closeAlert('success-alert')" class="absolute top-1 right-2 text-lg font-bold text-green-800 hover:text-green-600">&times;</button>
         {{ session('success') }}
     </div>
 @endif
 
 @if ($errors->any())
-    <div class="bg-red-100 text-red-800 px-4 py-2 rounded mb-4">
+    <div id="error-alert" class="bg-red-100 text-red-800 px-4 py-2 rounded mb-4 relative transition-opacity duration-300 ease-in-out">
+        <button onclick="closeAlert('error-alert')" class="absolute top-1 right-2 text-lg font-bold text-red-800 hover:text-red-600">&times;</button>
         @foreach ($errors->all() as $error)
             <div>{{ $error }}</div>
         @endforeach
     </div>
 @endif
+
